56accb5ab358394f422303b3e2e224b328923f67
morislaflame <andrey.uraltender@yandex.ru>
2025-01-22 22:45:51 +0500

    Razdacha

--- src/components/GameComponents/animations/dealCards.ts
+++ src/components/GameComponents/animations/dealCards.ts
@@ -17,24 +17,6 @@ export function animateFlip(oldState: FlipState, onComplete?: () => void) {
     duration: 0.9,
     absolute: true,
     stagger: 0.05,
-    // onEnter(elements) {
-    //   console.log('onEnter called with elements:', elements);
-    //   return gsap.from(elements, { 
-    //     opacity: 0, 
-    //     y: -30, 
-    //     stagger: 0.1, // Добавляем задержку 0.1 секунды между анимациями
-    //     ease: "power2.out" 
-    //   });
-    // },
-    // onLeave(elements) {
-    //   console.log('onLeave called with elements:', elements);
-    //   return gsap.to(elements, { 
-    //     opacity: 0, 
-    //     y: 30, 
-    //     stagger: 0.05,
-    //     ease: "power2.in" 
-    //   });
-    // },
     onComplete,
   });
 }
